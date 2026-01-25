e16ff823b3579ad225c82f2b3ab20f9e259ea0b8
Limesfield <kokorofree7@gmail.com>
2026-01-25 22:33:16 +0100

    ajout page contact, galerie, menu, account, connexion, editPassword, inscription et photos

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,6 +3,13 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/galerie", "La Galerie", "/pages/galerie.html"),
+    new Route("/connexion", "Connexion", "/pages/auth/connexion.html"),
+    new Route("/inscription", "Inscription", "/pages/auth/inscription.html"),
+    new Route("/account", "Mon Compte", "/pages/auth/account.html"),
+    new Route("/editPassword", "Modifier mon mot de passe", "/pages/auth/editPassword.html"),
+    new Route("/contact", "Contactez-nous", "/pages/contact.html"),
+    new Route("/menu", "Découvrez nos menus", "/pages/menu.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
